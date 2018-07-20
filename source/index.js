24ec14ddbe2f60493cfddbb56ed4ce1bfdad87de
Vladimir Kravchenko <Vladimir.Kravchenko@playtech.com>
2018-07-20 11:04:53 +0300

    Tests were added

--- source/index.js
+++ source/index.js
@@ -1,9 +1,28 @@
 var expect = require('expect');
 
 function counter(state, action) {
-    return state;
+    var newState = state;
+    switch(action.type) {
+        case "INCREMENT":
+            newState +=1;    
+        break;
+        case "DECREMENT":
+            newState -=1;    
+        break;
+    }
+    return newState;
 }
 
 expect(
-    counter(0, {})
+    counter(0, {type: ""})
 ).toEqual(0);
+
+expect(
+    counter(0, {type: "INCREMENT"})
+).toEqual(1);
+
+expect(
+    counter(1, {type: "DECREMENT"})
+).toEqual(0);
+
+console.log("Tests passed!");
