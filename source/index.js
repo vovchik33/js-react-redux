a9793d456f82cf90fddf02ea01e336e486094fa6
Vladimir Kravchenko <Vladimir.Kravchenko@playtech.com>
2018-07-20 11:13:26 +0300

    Redux store is used

--- source/index.js
+++ source/index.js
@@ -1,7 +1,8 @@
 var expect = require('expect');
+var redux = require('redux');
 
 function counter(state, action) {
-    var newState = state;
+    var newState = state?state:0;
     switch(action.type) {
         case "INCREMENT":
             newState +=1;    
@@ -12,6 +13,12 @@ function counter(state, action) {
     }
     return newState;
 }
+
+var store = redux.createStore(counter);
+store.subscribe(function() {
+    console.log("State was changed: "+store.getState());
+})
+store.dispatch({type: "INCREMENT"})
 
 expect(
     counter(0, {type: ""})
@@ -25,4 +32,13 @@ expect(
     counter(1, {type: "DECREMENT"})
 ).toEqual(0);
 
+expect(
+    counter(store.getState(), {type: "INCREMENT"})
+).toEqual(store.getState()+1);
+
+expect(
+    counter(store.getState(), {type: "DECREMENT"})
+).toEqual(store.getState()-1);
+
+console.log("Counter state:"+store.getState());
 console.log("Tests passed!");
